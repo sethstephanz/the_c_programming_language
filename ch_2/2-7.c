434ed9e1e2e4d56908d5069493e8c5738047576a
Seth Stephanz <sethstephanz@gmail.com>
2025-02-16 23:59:59 -0500

    Update 2-7 with more accurate comments

--- ch_2/2-7.c
+++ ch_2/2-7.c
@@ -5,14 +5,14 @@
 // + p (inclusive) inverted (assuming n < p) leaving other bits unchanged.
 
 int invert(uint32_t x, uint32_t p, uint32_t n) {
-    uint32_t mask = ((1U << (p - n + 1)) - 1) << n; // create mask for range p thru n (left to right)
+    uint32_t mask = ((1U << (p - n + 1)) - 1) << n; // create mask for range n thru p (r to l, inclusive)
     return x ^ mask;                                // XOR to flip bits in range
 }
 
 void print_binary(uint32_t num, uint32_t p, uint32_t n) {
     for (int i = sizeof(num) * 8 - 1; i >= 0; i--) {
         if (i <= p && i >= n) {
-            // print the inverted bits in green to assess output
+            // print the inverted bits in green to assess output visually
             printf("\033[32m%d\033[0m", (num >> i) & 1);
         } else {
             printf("%d", (num >> i) & 1);
@@ -23,7 +23,8 @@ void print_binary(uint32_t num, uint32_t p, uint32_t n) {
 
 int main() {
     uint32_t x = 0b1100101010101110001110101010;
-    // output:   0b1100101010101110000001001010
+
+    // this one does bits 5 thru 10 (from right) inclusive. so 6 bits total should be inverted
     int p = 10;
     int n = 5;
 
