e5f67a43ffd11400a17cf0e94998b939c209ff41
Seth Stephanz <sethstephanz@gmail.com>
2025-02-26 21:21:35 -0500

    Update 2-10 comments

--- ch_2/2-10.c
+++ ch_2/2-10.c
@@ -4,19 +4,14 @@
 // Write a function that converts uppercase letters to lowercase. Use a ternary operator.
 
 void to_lower(char *string) {
-    // getchar until newline
-    // if not alpha:     do nothing
-    // elif:             if it is not uppercase, do nothing
-    // else:             convert to lowercase
-
-    // loop thru string. while char not null
-    //      if not alpha ? not uppercase : convert to lowercase
+    // getchar until null/newline
+    //      if not alpha:     do nothing
+    //      elif:             if it is not uppercase, do nothing
+    //      else:             convert to lowercase
 
     for (int i = 0; string[i] != '\0'; i++) {
         // condition ? if_true : if_false;
         // note that condition has to be something that assesses to true or false
-        // it is the same as: if (condition) ? do this : else, do this
-
         // if the char is a letter and is uppercase ? convert to lower : don't do anything
         (isalpha(string[i]) && isupper(string[i])) ? string[i] = tolower(string[i]) : 0;
     }
